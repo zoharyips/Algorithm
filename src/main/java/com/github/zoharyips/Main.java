1e60b8c025c4e9d68b9e0409153265fe3a62dc24
叶卓豪 <yezhuohao@baijia.com>
2021-09-30 23:33:08 +0800

    init project

---
+++ src/main/java/com/github/zoharyips/Main.java
@@ -0,0 +1,14 @@
+package com.github.zoharyips;
+
+/**
+ * <h3>belief</h3>
+ *
+ * @author zohar
+ * @version 1.0
+ * 2021/9/30 23:25:57
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
